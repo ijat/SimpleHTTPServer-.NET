42965410a07db44c389296bb64fb7095acfffa26
Ijat <contact@ijat.my>
2019-06-06 13:53:59 +0800

    initial commit

---
+++ SimpleHTTPServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SimpleHTTPServer
+{
+    public class Program
+    {
+        static string rootPath = Directory.GetCurrentDirectory();
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string portStr = args.ElementAtOrDefault(0);
+            int port = String.IsNullOrEmpty(portStr) ? 8000 : Int32.Parse(portStr);
+            string privkeyPath = args.ElementAtOrDefault(1);
+            string privkeySecret = args.ElementAtOrDefault(2);
+
+            if (String.IsNullOrWhiteSpace(privkeyPath))
+                return WebHost.CreateDefaultBuilder(args)
+                        .UseUrls($"http://*:{port}")
+                        .UseContentRoot(path)
+                        .UseKestrel()
+                        .UseStartup<Startup>();
+            else
+                return WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(path)
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, port, listenOptions => {
+                        listenOptions.UseHttps(privkeyPath, privkeySecret);
+                    });
+                })
+                .UseStartup<Startup>();
+        }
+    }
+}
---
+++ SimpleHTTPServer/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleHTTPServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(
+            builder =>
+            {
+                builder.AddFilter("Microsoft", LogLevel.Error)
+                       .AddFilter("System", LogLevel.Error)
+                       .AddFilter("NToastNotify", LogLevel.Error)
+                       .AddConsole();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var options = new FileServerOptions()
+            {
+                FileProvider = env.ContentRootFileProvider,
+                RequestPath = String.Empty,
+                EnableDirectoryBrowsing = true,
+                EnableDefaultFiles = true
+            };
+
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+
+            app.UseFileServer(options);
+        }
+    }
+}
